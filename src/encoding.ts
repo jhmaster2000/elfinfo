8d4575aefeb838b6f43253fed14a583054a8ca32
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-10-13 04:57:55 -0300

    encoding functions

--- src/encoding.ts
+++ src/encoding.ts
@@ -36,8 +36,7 @@ if (typeof require !== undefined) {
                 return decoder.end(array.subarray(offset, offset + length));
             }
         }
-    } catch (e) {
-    }
+    } catch (e) {}
 }
 
 if (_decode === undefined && typeof TextDecoder !== 'undefined') {
@@ -47,6 +46,22 @@ if (_decode === undefined && typeof TextDecoder !== 'undefined') {
     }
 }
 
-export function encode(string: string, encoding: BufferEncoding = 'utf-8'): Buffer {
+function toHexSInt(bits: number, number: number): string {
+    if (number < 0) number = ((0x100 ** (bits / 8)) - 1) + number + 1;
+    return number.toString(16).toUpperCase();
+}
+
+export function encode(input: string | number, bytesAlign: number = 0, signed: boolean = false, encoding: BufferEncoding = 'utf-8'): Buffer {
+    if (typeof input === 'string') return encodeString(input, encoding);
+    if (typeof input === 'number') return encodeNumber(input, bytesAlign, signed);
+    throw new Error('Unexpected input type "' + typeof input + '".');
+}
+
+function encodeString(string: string, encoding: BufferEncoding = 'utf-8'): Buffer {
     return Buffer.from(string, encoding);
+}
+
+function encodeNumber(number: number, bytesAlign: number = 0, signed: boolean = false): Buffer {
+    const tmpstr: string = signed ? toHexSInt((bytesAlign * 8) || 8, number) : number.toString(16);
+    return Buffer.from(tmpstr.padStart(tmpstr.length + Number(tmpstr.length % 2 !== 0), '0').padStart(bytesAlign * 2, '0'), 'hex');
 }
